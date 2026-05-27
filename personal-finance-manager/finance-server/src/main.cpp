cee65055c1d8611bb91e1c8e323d3fb466c09df0
Kukreti <rucha.kukreti@hp.com>
2026-05-28 00:12:00 +0530

    Added the main file for server

--- personal-finance-manager/finance-server/src/main.cpp
+++ personal-finance-manager/finance-server/src/main.cpp
@@ -1,28 +1,44 @@
 #include "utils/ConfigLoader.h"
 #include "utils/Logger.h"
+#include "database/Database.h"
 
 #include <httplib.h>
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
+#include <fmt/core.h>
 #include <filesystem>
 
-int main() {
-    auto& config = utils::ConfigLoader::instance();
-    config.load("config/config.json");
+int main(int argc, char* argv[]) {
+    try {
+        std::string configPath = "config/config.json";
+        if (argc > 1) {
+            configPath = argv[1];
+        }
 
-    std::filesystem::create_directories("logs");
-    utils::Logger::init(config.logLevel(), config.logFile());
+        auto& config = utils::ConfigLoader::instance();
+        config.load(configPath);
 
-    httplib::Server server;
+        std::filesystem::create_directories("logs");
+        utils::Logger::init(config.logLevel(), config.logFile());
 
-    server.Get("/health", [](const httplib::Request&, httplib::Response& res) {
-        nlohmann::json body;
-        body["status"] = "ok";
-        res.set_content(body.dump(), "application/json");
-    });
+        auto& db = database::Database::instance();
+        db.connect(config.dbHost(), config.dbPort(), config.dbUser(), config.dbPassword(), config.dbName());
 
-    spdlog::info("Starting server on {}:{}", config.serverHost(), config.serverPort());
-    server.listen(config.serverHost(), config.serverPort());
+        httplib::Server server;
+
+        server.Get("/health", [&](const httplib::Request&, httplib::Response& res) {
+            nlohmann::json body;
+            body["status"] = "ok";
+            body["database"] = db.isConnected() ? "connected" : "disconnected";
+            res.set_content(body.dump(), "application/json");
+        });
+
+        spdlog::info("Starting server on {}:{}", config.serverHost(), config.serverPort());
+        server.listen(config.serverHost(), config.serverPort());
+    } catch (const std::exception& e) {
+        fmt::print(stderr, "Fatal: {}\n", e.what());
+        return 1;
+    }
 
     return 0;
 }
